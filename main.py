ac5629387d4ebc883fd3334a5aac0f9f9d765ba0
zmirandan <josemirandaneto48@gmail.com>
2025-02-13 23:29:22 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,30 @@
+def both_ends(s):
+    if len (s) < 2: # +++ SUA SOLUÇÃO +++
+        return ''
+    return s[:2] + s[-2:]
+
+# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(both_ends, 'spring', 'spng')
+    test(both_ends, 'Hello', 'Helo')
+    test(both_ends, 'a', '')
+    test(both_ends, 'xyz', 'xyyz')
